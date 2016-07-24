1977ee2ad419ae801993551b0a284edbf072eb34
Peter Froud <pfroud@gmail.com>
2016-07-24 01:01:22 -0700

    Python version crashes on Allez Jr

--- scripts to scrape/specialized/python version/scrape_specialized.py
+++ scripts to scrape/specialized/python version/scrape_specialized.py
@@ -3,25 +3,15 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 import requests
 import json
+import pickle
 
 indent = '   '
-
-
-class Version:
-    def __init__(self, version, price, frame, fork, rear_der):
-        self.rear_der = rear_der
-        self.fork = fork
-        self.frame = frame
-        self.price = price
-        self.version = version
+stop_after = 999
 
 
 def remove_duplicates_preserve_order(seq):
     """
     http://stackoverflow.com/a/480227
-
-    :param seq:
-    :return:
     """
     seen = set()
     seen_add = seen.add
@@ -33,10 +23,21 @@ def main():
     r.raise_for_status()
     soup = BeautifulSoup(r.text, 'html.parser')
 
+    models = []
+
+    count = 0
     for bike in soup('a', class_ = 'product-tile__anchor'):
+        if count >= stop_after:
+            break
         href = bike['href']
         if 'triathlon' not in href:
-            read_model(href)
+            models.append(read_model(href))
+        count += 1
+
+    with open('models.pickle', 'wb') as f:
+        pickle.dump(models, f)
+
+    # print_input_file(models)
 
 
 def read_model(absolute_url):
@@ -44,20 +45,26 @@ def read_model(absolute_url):
     r.raise_for_status()
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    print(soup.find('h1', class_ = 'grid-header__heading').string.strip())
+    model_name = soup.find('h1', class_ = 'grid-header__heading').string.strip()
+
+    print(model_name)
 
     version_a_tags = soup('a', class_ = 'colorway-tile__anchor-link')  # <a> tags
     version_hrefs = [x['href'] for x in version_a_tags]  # change <a> tags to hrefs
     # version_hrefs = list(set(version_hrefs))  # remove duplicates
     version_hrefs = remove_duplicates_preserve_order(version_hrefs)
 
+    versions = []
+
     for url in version_hrefs:
         if 'frameset' not in url and 'module' not in url and 'sbuild' not in url:
             # pprint(read_version(r.url + url))
-            read_version(r.url + url)
+            versions.append(read_version(r.url + url))
             # exit()
 
     print()
+
+    return {'name': model_name, 'versions': versions}
 
 
 def read_version(url):
@@ -68,7 +75,7 @@ def read_version(url):
     for h2 in soup('h2', class_ = 'pdp-hero__heading'):
         print(indent + h2.string)
 
-    return {
+    struct = {
         'version': soup.find('h2', class_ = 'pdp-hero__heading').string,
         'price': get_price(soup),
         'frame': get_spec(soup, "frame"),
@@ -77,13 +84,21 @@ def read_version(url):
         'der_rear': get_spec(soup, "rear derailleur")
     }
 
+    for key, value in struct.items():
+        struct[key] = str(struct[key])
+
+    return struct
+
 
 def get_spec(soup, spec_name):
-    return soup.find('h2', string = spec_name.upper()).parent.next_sibling.next_sibling.p.string
+    try:
+        return soup.find('h2', string = spec_name.upper()).parent.next_sibling.next_sibling.p.string
+    except AttributeError:
+        return "[{} not found]".format(spec_name)
 
 
 def get_price(soup):
-    price_data = soup.find('div', class_='js-select-container')['data-available-params']
+    price_data = soup.find('div', class_ = 'js-select-container')['data-available-params']
 
     replace = ['&quot;', '&quoquot;', '&ququot;']
     for target in replace:
@@ -103,4 +118,14 @@ def get_first_dict_item(d):
     return d[list(d)[0]]
 
 
+def print_input_file(models):
+    with open('output.txt', 'w+') as f:
+        for model in models:
+            f.write(model['name'])
+
+
+# with open('models.pickle', 'rb') as f:
+#     print_input_file(pickle.load(f))
+
 main()
+# read_model('/us/en/bikes/road/amira')
